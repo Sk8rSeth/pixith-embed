b738807869fb71aff662eb7f219e16432a2e53c1
Seth Howell <seth.howell@vuria.com>
2019-05-01 10:59:37 -0700

    made it into a working plugin

---
+++ pixith-embed.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @package Pixith-Embed
+*/
+/*
+Plugin Name: Pixith Embed
+Plugin URI: https://github.com/Sk8rSeth/pixith-embed
+Description: This plugin adds a shortcode for creating a button and modal adding embed DOM and tracking info for infographics.
+Version: 1.0.0
+Author: Seth Howell
+Author URI: https://seth-howell.com
+License: GPLv2 or later
+Text Domain: Pixith Embed
+*/
